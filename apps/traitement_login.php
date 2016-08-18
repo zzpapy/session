fbfe7e85c0b870502eb6b303a18ab29d2a8bd6d3
zzpapy <gregory.pace@hotmail.fr>
2016-08-18 16:56:18 +0200

    tutu

---
+++ apps/traitement_login.php
@@ -0,0 +1,45 @@
+<?php
+$json=file_get_contents("stock.json");
+$stock = json_decode($json, true);
+
+
+$username="";
+$password="";
+
+
+if (isset($_POST["submit"]))
+{
+	if ( !empty($_POST["username"]))
+	{ 
+		
+		$username=$_POST["username"];
+		$password=$_POST["password"];
+		$i=0;
+		while ($i<sizeof($stock))
+		{
+			if ($username==$stock[$i]["username"] && $password==$stock[$i]["password"])
+			{
+				$_SESSION["name"]=$stock[$i]["name"];
+				$_SESSION["username"]=$username;
+				
+				echo "aaaaaaa";
+				$_SESSION["email"]=$stock[$i]["email"];
+				var_dump($_SESSION["email"]);
+			}
+			$i++;
+		// header("Location: index.php?page=home");
+		}
+		exit; 
+		
+		
+			
+		
+		
+		}
+		
+
+}
+
+
+	// require ("views/login.phtml");
+?>
--- apps/traitement_register.php
+++ apps/traitement_register.php
@@ -1,22 +1,42 @@
 <?php
-if (!isset($_SESSION["submit"]))
+$name="";
+$email="";
+$username="";
+$password="";
+$confirm="";
+var_dump($_SESSION);
+if (isset($_POST["submit"]))
 {
-	$page="register";
-}
-else
-{
-	$_SESSION["name"]=$name;
-	$_SESSION["email"]=$email;
-	$_SESSION["username"]=$username;
-	$_SESSION["password"]=$password;
-	$_SESSION["confirm"]=$confirm;
-	var_dump($_SESSION);
-	echo "aaaaaa";
+	if ( !empty($_POST["name"]))
+	{ 
+		$name=$_POST["name"];
+		$email=$_POST["email"];
+		$username=$_POST["username"];
+		$password=$_POST["password"];
+		$confirm=$_POST["confirm"];
+		echo "aaaaaa";
+		$json=file_get_contents("stock.json");
+		$stock = json_decode($json, true);
+		$id = uniqid();
+		$stockInput = array(
+			"name" => $name,
+			"email" => $email,
+			"username" => $username,
+			"password" =>  $password,
+			"confirm" => $confirm,
+			
+		);
+		$stock[] = $stockInput;
+		// array_push($stock,$stockInput);
+		$json=json_encode($stock);
+		file_put_contents("stock.json", $json);
+		header("Location: index.php?page=login");
+		exit; 
+		}
+		
 
 }
 
 
-header("Location: index.php?page=login");
-exit; 
 	// require ("views/login.phtml");
 ?>
